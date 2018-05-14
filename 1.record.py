9235631b2efd7082dcfd5e65fa7cff0ac1c89f5e
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-05-14 17:07:32 -0400

    Record data in individual files every 100000 seconds to avoid giant file syndrome

--- 1.record.py
+++ 1.record.py
@@ -1,6 +1,7 @@
 # Simply record ADS-B messages - requires the rtl-sdr package to be installed
 # This also searches specifically for messages with downlink format 17 (10001)
 
+import os
 import time
 import subprocess
 
@@ -10,19 +11,32 @@ p = subprocess.Popen('rtl_adsb',
                      stdout=subprocess.PIPE,
                      stderr=subprocess.PIPE)
 
-with open('plane_data.txt', 'w') as f:
+if not os.path.exists('raw'):
+    os.mkdir('raw')
 
-    while True:
+fout = None
 
-        timestamp = time.time()
-        hex_data = p.stdout.readline().decode('ascii').strip()[1:-1]
+while True:
 
-        # Make sure the message is valid using the checksum
-        if int(pms.util.crc(hex_data), 2) != 0:
-            continue
+    timestamp = time.time()
+    hex_data = p.stdout.readline().decode('ascii').strip()[1:-1]
 
-        bin_data = "{0:0112b}".format(int(hex_data, 16))
+    # Make sure the message is valid using the checksum
+    if int(pms.util.crc(hex_data), 2) != 0:
+        continue
 
-        if bin_data[:5] == '10001':
-            f.write("{0:.3f} {1}\n".format(timestamp, hex_data))
-            f.flush()
+    bin_data = "{0:0112b}".format(int(hex_data, 16))
+
+    if bin_data[:5] == '10001':
+
+        filename = os.path.join('raw', str(int(timestamp / 100000)))
+
+        if fout is not None and fout.name != filename:
+            fout.close()
+            fout = None
+
+        if fout is None:
+            fout = open(filename, 'w')
+
+        fout.write("{0:.3f} {1}\n".format(timestamp, hex_data))
+        fout.flush()
