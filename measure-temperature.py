8486694ce4d2846eb9a1a53e03e6e1c2c28ef71b
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-05-14 18:05:27 -0400

    Make temperature measuring script output multiple files

--- measure-temperature.py
+++ measure-temperature.py
@@ -6,11 +6,28 @@ RAW_DIR = os.path.join(os.path.dirname(__file__), 'raw')
 if not os.path.exists(RAW_DIR):
     os.mkdir(RAW_DIR)
 
-TEMPERATURE_FILE = os.path.join(RAW_DIR, 'temperature.log')
+fout = None
 
-with open(TEMPERATURE_FILE, 'a') as fout:
-    while True:
-        temp = os.popen("vcgencmd measure_temp").readline()
+while True:
+
+    timestamp = time.time()
+
+    filename = os.path.join(RAW_DIR, str(int(timestamp / 100000)) + '_temp.log')
+
+    if fout is not None and fout.name != filename:
+        fout.close()
+        fout = None
+
+    if fout is None:
+        fout = open(filename, 'a')
+
+    temp = os.popen("vcgencmd measure_temp").readline()
+    try:
         value_temperature = temp.replace("temp=","").replace("'C", "").strip()
-        fout.write("{0} {1}\n".format(time.time(), value_temperature))
-        time.sleep(10)
+    except Exception:
+        continue
+
+    fout.write("{0} {1}\n".format(time.time(), value_temperature))
+    fout.close()
+
+    time.sleep(10)
